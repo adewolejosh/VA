3b4eb335a0e7077ef8fca28681839775f9689502
Joshua Adewole <adewole.josh@gmail.com>
2026-02-18 19:19:49 +0100

    chore: add extensive gitignore, and use ruff formatting

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 import whisper
 import google.generativeai as genai
@@ -16,7 +15,7 @@ ALLOWED_EXTENSIONS = {"wav", "mp3", "ogg", "flac", "m4a"}
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "")
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
@@ -24,8 +23,9 @@ genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/upload", methods=["POST"])
@@ -34,20 +34,20 @@ def upload_audio():
 
     if "file" not in files:
         return jsonify({"error": "please upload an audio file"}), 400
-    
+
     audio = request.files.get("file", "")
 
     if audio == "" or audio.filename == "":
         return jsonify({"error": "please select a valid file"}), 400
 
     if audio and allowed_file(audio.filename):
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], audio.filename)
+        filepath = os.path.join(app.config["UPLOAD_FOLDER"], audio.filename)
         audio.save(filepath)
 
         model = whisper.load_model("tiny")
         prompt = model.transcribe(filepath).get("text", "")
         return jsonify({"transcription": f"{prompt}"}), 200
-    
+
     return jsonify({"error": "can't process your request please try again later"}), 404
 
 
@@ -55,9 +55,9 @@ def upload_audio():
 def transcribe():
     data = request.json
 
-    if data is None or not "text" in data:
-        return jsonify({"error": f"input the transcribed text"}), 400
-    
+    if (data is None) or ("text" not in data):
+        return jsonify({"error": "input the transcribed text"}), 400
+
     prompt = data["text"]
     ai_model = genai.GenerativeModel("gemini-1.5-flash")
     response = ai_model.generate_content(str(prompt))
